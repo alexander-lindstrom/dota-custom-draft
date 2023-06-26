397624e4d87cffe570a9a42f54a468012e1a2ae6
Alexander Lindström <alexander.b.lindstrom@gmail.com>
2023-06-26 16:21:39 +0200

    fixed copy button

--- public/client.js
+++ public/client.js
@@ -22,10 +22,10 @@ captainButton.addEventListener('click', function(e){
 
 var copyButton = document.getElementById('copy_draft');
 copyButton.addEventListener('click', function(e){
-	setClipBoard();
+	copyTextToClipboard(getDraftString());
 });
 
-function setClipBoard(){
+function getDraftString(){
 	
 	const dire = document.getElementById("dire_pick").getElementsByTagName('*');
 	const radiant = document.getElementById("radiant_pick").getElementsByTagName('*');
@@ -43,10 +43,51 @@ function setClipBoard(){
 		str = str + dire[i].id + ", ";
     }
 	
-	const clipBoard = navigator.clipboard;
-	clipBoard.writeText(str).then(() => {
-		alert("Copied draft to clipboard!");
-	});
+	return str;
+}
+
+function copyTextToClipboard(text) {
+	
+	var textArea = document.createElement("textarea");
+
+	// Probably don't need all of this styling.
+	// Place in the top-left corner of screen regardless of scroll position.
+	textArea.style.position = 'fixed';
+	textArea.style.top = 0;
+	textArea.style.left = 0;
+
+	// Ensure it has a small width and height. Setting to 1px / 1em
+	// doesn't work as this gives a negative w/h on some browsers.
+	textArea.style.width = '2em';
+	textArea.style.height = '2em';
+
+	// We don't need padding, reducing the size if it does flash render.
+	textArea.style.padding = 0;
+
+	// Clean up any borders.
+	textArea.style.border = 'none';
+	textArea.style.outline = 'none';
+	textArea.style.boxShadow = 'none';
+
+	// Avoid flash of the white box if rendered for any reason.
+	textArea.style.background = 'transparent';
+
+
+	textArea.value = text;
+
+	document.body.appendChild(textArea);
+	textArea.focus();
+	textArea.select();
+
+	try{
+		var successful = document.execCommand('copy');
+		var msg = successful ? 'successful' : 'unsuccessful';
+	} 
+	catch (err){
+		console.log('Could not copy text to clipboard');
+	}
+
+	document.body.removeChild(textArea);
 }
 		
 //Event handling
